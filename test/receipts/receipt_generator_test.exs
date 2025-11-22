d7143171347d9a79e332c61b15d83a64ab2f3c38
Elliot Larson <me@elliot.la>
2025-11-22 15:41:44 -0700

    Create EEx receipt template from HTML design

---
+++ test/receipts/receipt_generator_test.exs
@@ -0,0 +1,8 @@
+defmodule Receipts.ReceiptGeneratorTest do
+  use ExUnit.Case
+  alias Receipts.ReceiptGenerator
+
+  describe "template rendering" do
+    # Tests will be added in next step
+  end
+end
