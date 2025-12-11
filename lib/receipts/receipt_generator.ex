b55bd6fbdfceff3a36e0ff3283c73569777cb8c9
Elliot Larson <me@elliot.la>
2025-12-11 13:41:30 -0700

    Add page number footers to multi-page receipts
    - Created receipt_footer.html.eex template for PDF footers
    - Updated generate_pdf to include page numbers in footer
    - Footer format: "Item #<number> page X of Y" for single line items
    - Footer format: "Item #<number> (<identifier>) page X of Y" for multiple line items
    - Footer uses matching sans-serif font and color

--- lib/receipts/receipt_generator.ex
+++ lib/receipts/receipt_generator.ex
@@ -6,6 +6,9 @@ defmodule Receipts.ReceiptGenerator do
   @external_resource "priv/templates/receipt.html.eex"
   @template File.read!("priv/templates/receipt.html.eex")
 
+  @external_resource "priv/templates/receipt_footer.html.eex"
+  @footer_template File.read!("priv/templates/receipt_footer.html.eex")
+
   @external_resource "priv/static/angel_charity_logo.svg"
   @logo_data_uri (fn ->
                     logo_content = File.read!("priv/static/angel_charity_logo.svg")
@@ -13,9 +16,51 @@ defmodule Receipts.ReceiptGenerator do
                     "data:image/svg+xml;base64,#{encoded}"
                   end).()
 
-  def generate_pdf(auction_item, output_path) do
-    html = render_html(auction_item)
-    ChromicPDF.print_to_pdf({:html, html}, output: output_path)
+  def generate_pdf(line_item, output_path) do
+    import Ecto.Query
+    alias Receipts.Repo
+    alias Receipts.LineItem
+
+    # Preload item for display
+    line_item =
+      if Ecto.assoc_loaded?(line_item.item) do
+        line_item
+      else
+        Repo.preload(line_item, :item)
+      end
+
+    html = render_html(line_item)
+    item_number = line_item.item.item_identifier
+
+    # Get line item count for this item
+    line_item_count =
+      if line_item.item_id do
+        from(li in LineItem, where: li.item_id == ^line_item.item_id)
+        |> Repo.aggregate(:count, :id)
+      else
+        1
+      end
+
+    # Build footer text with line item identifier if multiple line items
+    item_label = if line_item_count > 1 do
+      "Item ##{item_number} (#{line_item.identifier})"
+    else
+      "Item ##{item_number}"
+    end
+
+    # Render footer template with item label
+    footer_html = EEx.eval_string(@footer_template, assigns: %{item_label: item_label})
+
+    ChromicPDF.print_to_pdf(
+      {:html, html},
+      output: output_path,
+      print_to_pdf: %{
+        displayHeaderFooter: true,
+        headerTemplate: "<span></span>",
+        footerTemplate: footer_html,
+        marginBottom: 0.6
+      }
+    )
   end
 
   def save_html(auction_item, output_path) do
